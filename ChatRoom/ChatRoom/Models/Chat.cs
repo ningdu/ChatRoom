b0959b0d16e73a9965c691a3afcf3665885eeeea
Ning Du <ningtuffy@gmail.com>
2015-01-04 22:58:23 -0800

    built initial models

---
+++ ChatRoom/ChatRoom/Models/Chat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChatRoom.Models
+{
+    public class Chat
+    {
+        public Int32 ChatID { get; set; }
+
+        public DateTime Time { get; set; }
+       
+        [Required()]
+        [DataType(DataType.MultilineText)]
+        public String Message { get; set; }
+    }
+}
---
+++ ChatRoom/ChatRoom/Models/ChatContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChatRoom.Models
+{
+    public class ChatContext:DbContext 
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+    }
+}
